e777019082604245ab14510380d1f7f64335657b
minh205-M <23020550@vnu.edu.vn>
2024-10-07 16:32:29 +0700

    Add files via upload

---
+++ QLTrangtrai.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Dongvat {
+    String ten;
+
+    Dongvat(String ten) {
+        this.ten = ten;
+    }
+
+    abstract boolean boi();
+    abstract boolean dibo();
+}
+
+class Lon extends Dongvat {
+    Lon(String ten) {
+        super(ten);
+    }
+
+    @Override
+    boolean boi() {
+        return false;
+    }
+
+    @Override
+    boolean dibo() {
+        return true;
+    }
+}
+
+class Vit extends Dongvat {
+    Vit(String ten) {
+        super(ten);
+    }
+
+    @Override
+    boolean boi() {
+        return true;
+    }
+
+    @Override
+    boolean dibo() {
+        return true;
+    }
+}
+
+class Ca extends Dongvat {
+    Ca(String ten) {
+        super(ten);
+    }
+
+    @Override
+    boolean boi() {
+        return true;
+    }
+
+    @Override
+    boolean dibo() {
+        return false;
+    }
+}
+
+public class QLTrangtrai {
+    public static void main(String[] args) {
+        List<Dongvat> dongvat = new ArrayList<>();
+        dongvat.add(new Lon("Piggy"));
+        dongvat.add(new Vit("Ducky"));
+        dongvat.add(new Ca("Goldie"));
+
+        printAnimalsThatCanSwimOrWalk(dongvat);
+    }
+
+    public static void printAnimalsThatCanSwimOrWalk(List<Dongvat> dongvat) {
+        System.out.println("Animals that can swim or walk:");
+        for (Dongvat dongvat : dongvat) {
+            if (dongvat.boi() || dongvat.dibo()) {
+                System.out.println(dongvat.ten);
+            }
+        }
+    }
+}
